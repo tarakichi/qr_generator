253b3b8a32821008fe99045192d195b4eb20383f
tarakichi <tarakichi48@gmail.com>
2025-07-20 22:08:32 +0900

    specification change

--- src/App.tsx
+++ src/App.tsx
@@ -42,9 +42,6 @@ function App() {
   const handleSave = async () => {
     const id = await addQRData(currentValue, labelList);
     console.log("保存完了 ID:", id);
-    setCurrentValue(generateRandomString());
-    setLabelList([]);
-    resetLabelInput();
   }
 
   const handleDelete = async (id: string) => {
@@ -76,11 +73,11 @@ function App() {
     <div className="w-screen flex flex-col items-center p-6 space-y-6">
       <h1 className="text-xl font-bold">QR Generator with Save/Load</h1>
       <div className="w-full flex gap-2">
-        <div className="w-1/2 flex flex-col items-center border border-gray-200 p-4">
+        <div className="w-1/2 flex flex-col items-center border border-gray-200 p-4 bg-white">
             <QRCode value={currentValue}/>
             <p className="mt-2 text-xs text-gray-600">Value: {currentValue}</p>
         </div>
-        <div className="w-1/2 flex flex-col items-center border border-gray-200 p-4 gap-2">
+        <div className="w-1/2 flex flex-col items-center border border-gray-200 p-4 gap-2 bg-white">
           <input
             type="text"
             value={labelInput.name}
@@ -181,6 +178,133 @@ function App() {
       </div>
 
       <img src="/GTKvlqxbQAIZ4jY.jfif" alt="体格表" />
+
+      <table className="bg-white">
+        <caption>
+          体格表
+          </caption>
+          <thead>
+            <tr>
+              <th colSpan={4}></th>
+              <th colSpan={2}>回避0</th>
+              <th colSpan={2}>回避3</th>
+            </tr>
+            <tr>
+              <th colSpan={4}></th>
+              <th>最速</th>
+              <th>最大</th>
+              <th>最速</th>
+              <th>最大</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th colSpan={4}>アンテナなし</th>
+              <td>29</td>
+              <td>40</td>
+              <td>24</td>
+              <td>37</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>全体攻撃、眠りマヒ毒、弱体系</th>
+              <td>27</td>
+              <td>37</td>
+              <td>22</td>
+              <td>35</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>単体攻撃（ひのたまetc...）</th>
+              <td>24</td>
+              <td>33</td>
+              <td>19</td>
+              <td>30</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>三体攻撃（ばくはつetc...）</th>
+              <td>25</td>
+              <td>34</td>
+              <td>20</td>
+              <td>32</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>めざめる、しびれとる</th>
+              <td>35</td>
+              <td>46</td>
+              <td>30</td>
+              <td>43</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>げどく</th>
+              <td>33</td>
+              <td>44</td>
+              <td>28</td>
+              <td>41</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>単体増強</th>
+              <td>27</td>
+              <td>36</td>
+              <td>22</td>
+              <td>33</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>全体増強</th>
+              <td>27</td>
+              <td>35</td>
+              <td>23</td>
+              <td>33</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>単体回復</th>
+              <td>31</td>
+              <td>42</td>
+              <td>26</td>
+              <td>39</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>単体復活</th>
+              <td>33</td>
+              <td>44</td>
+              <td>28</td>
+              <td>41</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>みんな回復</th>
+              <td>34</td>
+              <td>45</td>
+              <td>29</td>
+              <td>42</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>みんな復活</th>
+              <td>36</td>
+              <td>46</td>
+              <td>31</td>
+              <td>44</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>たくわえる</th>
+              <td>34</td>
+              <td>45</td>
+              <td>29</td>
+              <td>42</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>なおす</th>
+              <td>32</td>
+              <td>43</td>
+              <td>27</td>
+              <td>40</td>
+            </tr>
+            <tr>
+              <th colSpan={4}>ステルス</th>
+              <td>19</td>
+              <td>27</td>
+              <td>15</td>
+              <td>25</td>
+            </tr>
+          </tbody>
+      </table>
 
       <div className="w-full max-w-4xl">
         <h2 className="font-semibold mt-4">保存済みQRコード:</h2>
