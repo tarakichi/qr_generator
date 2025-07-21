a329c17744ea1268d0fec8d937d8247ac6da9814
tarakichi <tarakichi48@gmail.com>
2025-07-22 02:50:06 +0900

    save changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from './firebase';
 import { adminUIDs } from './constants/adminUIDs';
 import { GoogleIcon } from './components/GoogleIcon';
+import Select from 'react-select/base';
 
 function generateRandomString(length = 40) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -99,63 +100,73 @@ function App() {
     setLabelList(labelList.filter((_, i) => i !== index));
   }
 
+  const colorSelectOptions = colorOptions.map((c) => ({
+    value: c.id,
+    label: c.label,
+  }));
+
   if (loading) return <div>読み込み中...</div>
 
   return (
     <div className="w-screen flex flex-col items-center p-6 space-y-6">
-      <h1 className="text-xl font-bold text-zinc-200 font-notosans">QRコードジェネレータ</h1>
+      <h1 className="text-xl font-bold text-zinc-200">QRコードジェネレータ</h1>
       <div className="w-1/2 bg-white p-4 rounded border border-zinc-200">
         {user ? (
           <div className="flex justify-center items-center gap-4">
             <p className="text-zinc-700">こんにちは、{user.displayName || user.email}</p>
             <button
               onClick={handleLogout}
-              className="flex items-center justify-center space-x-3 border border-zinc-200 px-4 py-2 rounded shadow hover:shadow-lg transition bg-white text-zinc-300"
+              className="flex items-center justify-center space-x-3 border border-zinc-200 px-4 py-2 rounded shadow hover:shadow-lg transition bg-white"
             >
-              <span className="text-sm font-bold font-notosans">ログアウト</span>
+              <span className="text-sm font-bold text-zinc-300">ログアウト</span>
             </button>
           </div>
         ) : (
           <div className="flex flex-col items-center">
             <button
               onClick={handleLogin}
-              className="flex items-center justify-center space-x-3 border border-zinc-200 px-4 py-2 rounded shadow hover:shadow-lg transition bg-white text-zinc-300"
+              className="flex items-center justify-center space-x-3 border border-zinc-200 px-4 py-2 rounded shadow hover:shadow-lg transition bg-white"
             >
               <GoogleIcon />
-              <span className="text-sm font-bold font-notosans">Sign in with Google</span>
+              <span className="text-sm font-bold text-zinc-300">Googleでログイン</span>
             </button>
           </div>
         )}
       </div>
       <div className="w-full flex gap-2">
-        <div className="w-1/2 flex flex-col items-center border border-zinc-200 p-4 bg-white rounded">
+        <div className="w-1/2 flex flex-col items-center border border-zinc-200 p-4 bg-white rounded gap-2">
             <QRCode value={currentValue}/>
-            <p className="mt-2 text-xs text-gray-600">Value: {currentValue}</p>
+            <p className="text-xs text-zinc-300">Value: {currentValue}</p>
+            <div className="flex justify-center gap-2">
+              <button onClick={handleNext} className="bg-blue-500 text-white px-3 py-1 rounded">ランダム生成</button>
+            </div>
         </div>
         <div className="w-1/2 flex flex-col items-center border border-zinc-200 p-4 gap-2 bg-white rounded">
           <input
             type="text"
             value={labelInput.name}
             onChange={(e) => setLabelInput({ ...labelInput, name: e.target.value })}
-            className="w-3/4 bg-white border text-center rounded border-zinc-200"
+            className="w-3/4 bg-white border text-center rounded border-zinc-200 text-zinc-700"
             placeholder="名前"
           />
 
           <select
             value={labelInput.antenna || ""}
             onChange={(e) => setLabelInput({ ...labelInput, antenna: e.target.value as AntennaID })}
-            className="w-3/4 bg-white border text-center rounded border-zinc-200"
+            className="w-3/4 bg-white border text-center rounded border-zinc-200 text-zinc-700"
           >
             <option value="">(アンテナ)</option>
             {antennaOptions.map(opt => (
-              <option key={opt.id} value={opt.id}>{opt.label}</option>
+              <option key={opt.id} value={opt.id}>
+                {opt.label}
+              </option>
             ))}
           </select>
 
           <select
             value={labelInput.head || ""}
             onChange={(e) => setLabelInput({ ...labelInput, head: e.target.value as HeadID })}
-            className="w-3/4 bg-white border text-center rounded border-zinc-200"
+            className="w-3/4 bg-white border text-center rounded border-zinc-200 text-zinc-700"
           >
             <option value="">(頭の形)</option>
             {headOptions.map(opt => (
@@ -166,16 +177,26 @@ function App() {
           <select
             value={labelInput.color || ""}
             onChange={(e) => setLabelInput({ ...labelInput, color: e.target.value as ColorID })}
-            className="w-3/4 bg-white border text-center rounded border-zinc-200"
+            className="w-3/4 bg-white border text-center rounded border-zinc-200 text-zinc-700"
           >
             <option value="">(色)</option>
             {colorOptions.map(opt => (
               <option key={opt.id} value={opt.id}>{opt.label}</option>
             ))}
           </select>
+
+          <Select
+            options={colorSelectOptions}
+            value={colorSelectOptions.find((o) => o.value === labelInput.color)}
+            onChange={(selected) =>
+              setLabelInput({ ...labelInput, color: selected?.value as ColorID})
+            }
+            placeholder="色を選択してください"
+            isClearable
+          />
           
           <div className="w-3/4 bg-white flex justify-center items-center space-x-2 rounded">
-            <label htmlFor="hp">&#xff28;&#xff30;:</label>
+            <label htmlFor="hp" className="text-zinc-700">&#xff28;&#xff30;:</label>
             <input
               id="hp"
               type="range"
@@ -189,7 +210,7 @@ function App() {
           </div>
           
           <div className="w-3/4 bg-white flex justify-center items-center space-x-2 rounded">
-            <label htmlFor="evasion">回避:</label>
+            <label htmlFor="evasion" className="text-zinc-700">回避:</label>
             <input
               id="evasion"
               type="range"
@@ -205,17 +226,16 @@ function App() {
           <textarea
             value={labelInput.notes}
             onChange={(e) => setLabelInput({ ...labelInput, notes: e.target.value })}
-            className="w-3/4 bg-white border text-center rounded border-zinc-200"
+            className="w-3/4 bg-white border text-center rounded border-zinc-200 text-zinc-700"
             placeholder="メモ"
           />
           <div className="flex justify-center gap-2">
             <button onClick={handleAddLabel} className="bg-gray-500 text-white px-3 py-1 rounded">ラベル追加</button>
             <button onClick={handleSave} className="bg-green-500 text-white px-3 py-1 rounded">保存</button>
-            <button onClick={handleNext} className="bg-blue-500 text-white px-3 py-1 rounded">次へ</button>
           </div>
           <div className="w-3/4">
             {labelList.map((lbl, index) => (
-              <div key={index} className="border relative border-gray-200 p-4">
+              <div key={index} className="border relative border-zinc-200 p-4">
                 <button onClick={() => handleRemoveLabel(index)} className="absolute top-0 right-2 text-xs text-red-500">
                   x
                 </button>
@@ -241,11 +261,11 @@ function App() {
             <li key={item.id} className="flex w-full items-start border p-4 border-gray-200">
               <div className="w-2/3">
                 <QRCode value={item.value} size={64}/>
-                <div className="mt-2 text-sm text-cyan-700">Value: {item.value}</div>
+                <div className="mt-2 text-sm text-zinc-300">Value: {item.value}</div>
               </div>
-              <div className="flex w-1/3 space-y-1">
+              <div className="flex w-1/3 space-y-1 overflow-x-auto">
                 {item.label.map((lbl, index) => (
-                  <div key={index} className={`p-2 rounded bcgd-${lbl.color}`}>
+                  <div key={index} className={`w-full p-2 rounded bcgd-${lbl.color}`}>
                     <div>名前: {lbl.name}</div>
                     <div>アンテナ: {lbl.antenna && antennaOptions[Number(lbl.antenna)].label}</div>
                     <div>頭の形: {lbl.head && headOptions[Number(lbl.head)].label}</div>
