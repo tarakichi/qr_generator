ae28a988837f04a7dc6850bf1aab8dfaa54221b0
tarakichi <tarakichi48@gmail.com>
2025-07-24 19:21:03 +0900

    add deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://tarakichi.github.io/qr_generator",
   plugins: [
     react(),
     tailwindcss(),
